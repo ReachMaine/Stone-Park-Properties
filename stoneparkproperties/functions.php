bb22d23130db1ac9d4dbe140d111b36078453ccc
linda@reachmaine.com <linda@reachmaine.com>
2018-12-14 14:57:32 -0500

    Add content bottom widget area & styling footer widgets

--- stoneparkproperties/functions.php
+++ stoneparkproperties/functions.php
@@ -4,15 +4,18 @@
  * Functions.php
  *
  * ===== NOTES ==================================================================
- * 
- * Unlike style.css, the functions.php of a child theme does not override its 
- * counterpart from the parent. Instead, it is loaded in addition to the parent's 
+ *
+ * Unlike style.css, the functions.php of a child theme does not override its
+ * counterpart from the parent. Instead, it is loaded in addition to the parent's
  * functions.php. (Specifically, it is loaded right before the parent's file.)
- * 
- * 
+ *
+ *
  * =============================================================================== */
- 
+
 function divichild_enqueue_scripts() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
+
+require_once(get_stylesheet_directory().'/custom/reach-CTAs.php');
+require_once(get_stylesheet_directory().'/custom/divi.php');
